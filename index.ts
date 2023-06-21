ded81ca0cc49d500268630c4a306940510f3eeb4
Arie Schwartzman <ariesch.microsoft.com>
2023-06-21 14:37:20 +0300

    wip

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 // parse body into json
 import bodyParser from 'body-parser';
-// pare query string parameters
+import axios from 'axios';
+// import moment.js
+import moment from 'moment';
 
+// pare query string parameters
 
 dotenv.config();
 
@@ -33,16 +36,102 @@ app.get('/logo.png', (req, res) => {
   res.sendFile('logo.png', { root: __dirname });
 });
 
-app.post('/clinicaltrials/:patientId', (req: Request, res: Response) => {
+app.post('/clinicaltrials/:patientId', async (req: Request, res: Response) => {
   const patientId = req.params.patientId;
   const body = req.body;
-  console.log(`[server]: patientId: ${patientId}`);
-  const response = [
-    "Trial 1",
-    "Trial 2",
-    "Trial 3"
-  ];
-  res.send(response);
+  console.log(`[server]: body: ${JSON.stringify(body)}`);
+
+  // call using Axios for Azure Health Insights
+  const baseUrl = process.env.AZURE_HEALTH_INSIGHTS_URL;
+
+  const url = `${baseUrl}/healthinsights/trialmatcher/jobs?api-version=2023-03-01-preview`
+  
+  // Create a JSON payload
+
+  // calculate the birth date from age in years use moment.js
+  const birthDate = moment().subtract(body.age, 'years').format('YYYY-MM-DD');
+
+  const payload = {
+    configuration: {clinicalTrials:
+      {
+        registryFilters: [{
+            conditions: [
+               body.query
+            ],
+            phases: [
+               "phase2",
+               "phase3"
+            ],
+            sources: [
+               "clinicaltrials.gov"
+            ],
+            facilityLocations: [
+               {
+                  "countryOrRegion": "United States",
+                  "city": body.location || "Birmingham",
+                  "state": "AL"
+               }
+            ]
+        }]
+      }
+    },
+    patients: [
+      {
+         id: patientId,
+         info: {
+            sex: body.gender,
+            birthDate: birthDate,
+            clinicalInfo: [
+               {
+                  system: "http://www.nlm.nih.gov/research/umls",
+                  code: "C0012544",
+                  name: "Biphosphonates",
+                  value: "true"
+               }               
+            ]
+         }
+      }
+   ]
+  }
+
+  try {
+    const ahi_resposne = await axios.post(url, payload, 
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': process.env.AZURE_HEALTH_INSIGHTS_KEY
+        }  
+      });
+      const location = ahi_resposne.headers['operation-location'];
+      console.log(`[server]: ahi_resposne: ${location}`);
+      let done = false;
+      let tries = 0;
+      let jobResponse;
+      while (!done && tries < 10) {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        jobResponse = await axios.get(location, {headers: {'Ocp-Apim-Subscription-Key': process.env.AZURE_HEALTH_INSIGHTS_KEY}});
+        if (jobResponse.data.status === 'succeeded') {
+          done = true;
+          continue;
+        }
+        tries++;
+      }
+      // take 5 first trials
+      if (jobResponse) {
+        // filter out only eligible trials
+        const eligible = jobResponse.data.results.patients[0].inferences.filter((el: any) => el.value === "Eligible"); 
+        const trials = eligible.slice(0, 5);
+        const output = trials.map((trial: any) => { 
+          return trial.id
+        });
+        res.send(output);
+      }
+  }
+  catch (error) {
+    console.log(`[server]: error: ${error}`);
+    res.status(400);
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
